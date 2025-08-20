a5b47f6c4b9ab1cb89ceb1633e8f001f51c9cfec
Mehedi0101 <mehedih2909@gmail.com>
2025-08-20 19:30:58 +0600

    login and register page is added

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -2,16 +2,20 @@ import { Outlet } from "react-router-dom";
 import bgImg from "../assets/auth-bg-img.jpg";
 import Header from "../components/auth_layout/Header";
 import Footer from "../components/auth_layout/Footer";
+import WelcomeText from "../components/auth_layout/WelcomeText";
 
 const AuthLayout = () => {
     return (
         <div 
-        className="h-screen bg-cover bg-no-repeat bg-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-blend-multiply" 
+        className="min-h-screen bg-cover bg-no-repeat bg-center bg-fixed font-open-sans" 
         style={{ backgroundImage: `linear-gradient(to right, rgba(0, 0, 0, 1), rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 1)), url(${bgImg})`}}
         >
-            <section className="w-11/12 mx-auto">
+            <section className="max-w-7xl w-11/12 mx-auto">
                 <Header></Header>
+                <WelcomeText></WelcomeText>
+
                 <Outlet></Outlet>
+
                 <Footer></Footer>
             </section>
         </div>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,85 @@
+import { Link } from "react-router-dom";
+
 const Login = () => {
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+    };
+
     return (
-        <div>
-            Login
+        <div className="flex items-center justify-center px-4 text-sm md:text-base">
+            <div className="w-full max-w-md">
+                <div className="bg-[#ffffffe3] rounded-2xl shadow-xl p-8">
+                    {/* Form */}
+                    <form onSubmit={onSubmit} className="space-y-5">
+                        {/* email */}
+                        <div>
+                            <input
+                                id="email"
+                                name="email"
+                                type="email"
+                                required
+                                className="mt-1 w-full px-3 py-2 border-b-2 border-gray-300 text-gray-900 focus:outline-none"
+                                placeholder="Email"
+                            />
+                        </div>
+
+                        {/* password */}
+                        <div>
+                            <input
+                                id="password"
+                                name="password"
+                                type="password"
+                                autoComplete="current-password"
+                                required
+                                className="mt-1 w-full px-3 py-2 border-b-2 border-gray-300 text-gray-900 focus:outline-none"
+                                placeholder="Password"
+                            />
+                        </div>
+
+                        <div className="flex flex-col gap-3 sm:gap-0 sm:flex-row justify-between">
+                            {/* Remember me */}
+                            <div className="flex items-center">
+                                <input
+                                    id="remember"
+                                    name="remember"
+                                    type="checkbox"
+                                    className="h-4 w-4 rounded border-gray-300 text-primary focus:ring-primary"
+                                />
+                                <label htmlFor="remember" className="ml-2 block text-sm text-gray-700">
+                                    Remember me
+                                </label>
+                            </div>
+
+                            {/* forgot password */}
+                            <Link to="/forgot-password" className="text-sm text-primary hover:underline">
+                                Forgot password?
+                            </Link>
+                        </div>
+
+                        <button
+                            type="submit"
+                            className="w-full rounded-lg bg-primary px-4 py-2.5 text-white font-medium hover:bg-primary focus:outline-none focus:scale-95 transition-all ease-linear cursor-pointer"
+                        >
+                            Log into your account
+                        </button>
+                    </form>
+
+                    <div className="my-6 flex items-center">
+                        <div className="h-px flex-1 bg-gray-400" />
+                        <span className="px-3 text-xs uppercase tracking-wide text-gray-600">or</span>
+                        <div className="h-px flex-1 bg-gray-400" />
+                    </div>
+
+                    {/* Registration link */}
+                    <div className="text-center text-sm">
+                        <span className="text-gray-600">Don't have an account? </span>
+                        <Link to="/auth/register" className="font-medium text-primary hover:underline">
+                            Create an account
+                        </Link>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
