997d2c3dabcab8ca953980700df3973ab138bc3e
Mehedi0101 <mehedi29121998@gmail.com>
2025-09-18 18:52:10 +0600

    event details page is added excluding update and delete event button's functionality

---
+++ src/components/user_layout/UserEvents/EventCard.jsx
@@ -0,0 +1,120 @@
+import { format } from "date-fns";
+import defaultEventBanner from "../../../assets/default_event_banner.jpg";
+import hourFormatConverter from "../../../functions/formatTimeString";
+import { FaRegStar, FaStar } from "react-icons/fa";
+import { useContext, useState } from "react";
+import axios from "axios";
+import AuthContext from "../../../contexts/AuthContext";
+import toast from "react-hot-toast";
+import { ClipLoader } from "react-spinners";
+import defaultUser from "../../../assets/default_user.jpg";
+import { Link } from "react-router-dom";
+
+const EventCard = ({ event, isMyEvent = false }) => {
+
+    const { userDetails } = useContext(AuthContext);
+
+    const [eventData, setEventData] = useState(event);
+    const [loading, setLoading] = useState(false);
+    const day = format(eventData?.date, "d");
+    const month = format(eventData?.date, "MMM").toUpperCase();
+
+    const handleToggleInterested = (e) => {
+        e.preventDefault();
+
+        setLoading(true);
+        axios.patch(`http://localhost:5000/events/interested/${eventData?._id}`, { userId: userDetails?._id })
+            .then(data => {
+                setEventData(data.data)
+                setLoading(false);
+            })
+            .catch(() => {
+                toast.error("Something went wrong")
+                setLoading(false);
+            })
+    }
+
+    console.log(eventData);
+
+    return (
+        <Link to={`/events/${event?._id}`} className="rounded-xl flex flex-col shadow-lg hover:shadow-xl cursor-pointer">
+            {/* ---------- Banner ---------- */}
+            <div
+                className="h-40 bg-cover bg-no-repeat bg-center rounded-t-xl"
+                style={{
+                    backgroundImage: `linear-gradient(to right, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.7)), url(${eventData?.banner || defaultEventBanner})`,
+                }}
+            >
+                <div className="bg-white inline-block px-2 py-1 rounded m-4 text-center text-primary font-poppins">
+                    <p className="text-xl font-semibold leading-6">{day}</p>
+                    <p className="text-lg leading-6">{month}</p>
+                </div>
+            </div>
+
+            {/* ---------- Content (fills space) ---------- */}
+            <div className="flex flex-col justify-between flex-1 mx-4 py-3">
+                <div>
+                    <div className="flex items-center justify-between">
+                        <h3
+                            className="text-lg text-dark font-semibold font-poppins"
+                            title={eventData.title} // tooltip full title
+                        >
+                            {eventData.title?.length > 40
+                                ? eventData.title.slice(0, 40) + "..."
+                                : eventData.title}
+                        </h3>
+                        <p
+                            className={`text-xs font-medium ${eventData.type === "Online" ? "text-green-600" : "text-blue-600"
+                                }`}
+                        >
+                            {eventData.type}
+                        </p>
+                    </div>
+                    <p className="text-sm text-slate-500">
+                        {hourFormatConverter(eventData?.timeRange?.start)} -{" "}
+                        {hourFormatConverter(eventData?.timeRange?.end)}
+                    </p>
+                </div>
+
+                {/* ---------- Always sticks to bottom ---------- */}
+                <div className="flex gap-1 justify-between items-center mt-4 h-8">
+                    <div className="text-primary text-sm hidden min-[200px]:flex items-center gap-2 font-semibold font-poppins">
+                        <div className="flex -space-x-3">
+                            {
+                                eventData?.interestedPreview?.map((img, idx) => {
+                                    return (
+                                        <img key={idx} className="w-8 h-8 rounded-full object-cover border-2 border-white" src={img || defaultUser} />
+                                    )
+                                })
+                            }
+                        </div>
+                        {eventData?.interestedCount > 3 && `+${eventData?.interestedCount - 3} `}
+                        {eventData?.interestedCount > 0 && "Interested"}
+                    </div>
+
+                    {/* ---------- is interested section hidden if current user is event creator ---------- */}
+                    <div className={`${isMyEvent && "hidden"}`}>
+                        {
+                            loading
+                                ?
+                                <ClipLoader
+                                    color="#f59e0b"
+                                    size={15}
+                                    aria-label="Loading Spinner"
+                                    data-testid="loader"
+                                />
+                                :
+                                eventData?.isInterested
+                                    ?
+                                    <FaStar onClick={handleToggleInterested} className="text-amber-500 text-xl cursor-pointer" />
+                                    :
+                                    <FaRegStar onClick={handleToggleInterested} className="text-amber-500 text-xl cursor-pointer" />
+                        }
+                    </div>
+                </div>
+            </div>
+        </Link>
+    );
+};
+
+export default EventCard;
--- src/pages/user_layout/UserEvents.jsx
+++ src/pages/user_layout/UserEvents.jsx
@@ -1,12 +1,30 @@
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import UserHeader from "../../components/user_layout/shared/UserHeader";
 import { Link } from "react-router-dom";
 import PurpleButton from "../../components/shared/buttons/PurpleButton";
 import { IoMdAddCircleOutline } from "react-icons/io";
+import axios from "axios";
+import AuthContext from "../../contexts/AuthContext";
+import EventCard from "../../components/user_layout/UserEvents/EventCard";
 
 const UserEvents = () => {
 
+    const { userDetails } = useContext(AuthContext);
+
+    const [upcomingEvents, setUpcomingEvents] = useState([]);
     const [myEvents, setMyEvents] = useState([]);
+
+    useEffect(() => {
+        // ---------- get all upcoming events ----------
+        userDetails?._id && axios.get(`http://localhost:5000/events/all/${userDetails?._id}`)
+            .then(data => setUpcomingEvents(data.data))
+            .catch(() => setUpcomingEvents([]))
+
+        // ---------- get all my events ----------
+        userDetails?._id && axios.get(`http://localhost:5000/events/user/${userDetails?._id}`)
+            .then(data => setMyEvents(data.data))
+            .catch(() => setMyEvents([]))
+    }, [userDetails])
 
     return (
         <div>
@@ -32,16 +50,36 @@ const UserEvents = () => {
                         </Link>
                     </div>
 
-                    <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 mt-6">
+                    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-10 sm:gap-5 mt-6">
                         {
                             // ---------- check if myEvents is empty or not ---------- 
                             myEvents?.length > 0 ?
                                 // ---------- not empty ---------- 
-                                myEvents.map(jobPost => <JobCard key={jobPost._id} jobPost={jobPost}></JobCard>)
+                                myEvents.map(event => <EventCard key={event._id} event={event} isMyEvent={true}></EventCard>)
                                 :
                                 // ---------- empty ---------- 
-                                <div className="col-span-1 lg:col-span-2 min-h-40 flex justify-center items-center text-base sm:text-lg font-bold text-center">
+                                <div className="col-span-1 sm:col-span-2 lg:col-span-3 2xl:col-span-4 min-h-40 flex justify-center items-center text-base sm:text-lg font-bold text-center">
                                     You haven't created any events yet.
+                                </div>
+                        }
+                    </div>
+                </div>
+
+                {/* ---------- available job display section ---------- */}
+                <div className="mt-12">
+                    <h2 className="font-poppins text-xl font-bold text-dark">Upcoming Events</h2>
+
+                    {/* ---------- jobs container ---------- */}
+                    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-10 sm:gap-5 mt-6">
+                        {
+                            // ---------- check if allJobs is empty or not ---------- 
+                            upcomingEvents?.length > 0 ?
+                                // ---------- not empty ---------- 
+                                upcomingEvents.map(event => <EventCard key={event._id} event={event}></EventCard>)
+                                :
+                                // ---------- empty ---------- 
+                                <div className="col-span-1 sm:col-span-2 lg:col-span-3 2xl:col-span-4 min-h-40 flex justify-center items-center text-base sm:text-lg font-bold text-center">
+                                    No job opportunities available right now.
                                 </div>
                         }
                     </div>
